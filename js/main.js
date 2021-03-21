e3d3078b7691330adbd3fe94d14ef5a4d1340c31
Arij Khaldi <khaldi.arij9@gmail.com>
2021-03-21 18:39:23 +0100

    first application

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var section=Array.from(document.getElementsByClassName("item"))
+
+section.forEach(function(item){
+
+    console.log(item);
+})
+var qte=Array.from(document.getElementsByClassName("quantityy"))
+var price=Array.from(document.getElementsByClassName("price"))
+var total=document.getElementById("total")
+var delt=Array.from(document.getElementsByClassName("btn-btn"))
+var btnplus=Array.from(document.getElementsByClassName("button-plus"))
+var btnminus=Array.from(document.getElementsByClassName("button-minus"))
+var heart=Array.from(document.getElementsByClassName("far fa-heart"))
+console.log(delt)
+
+
+for(let i=0; i < delt.length; i++){
+    let buttonremove = delt[i] ;
+    buttonremove.addEventListener('click' , function(event){
+        let buttonclick = event.target 
+        buttonclick.parentElement.parentElement.remove() 
+   
+        price[i].innerHTML= 0 ;
+        calcul()
+})
+}
+
+
+function calcul(){
+    var somme=0;
+    for(let i=0;i<price.length;i++){
+    
+        somme=somme+Number(price[i].innerHTML)*Number(qte[i].innerHTML)
+
+    }
+    total.innerHTML=somme
+    console.log(somme)
+    return somme;
+
+}
+
+
+
+for(let i=0; i<btnplus.length;i++){
+    btnplus[i].addEventListener('click',function(){
+        qte[i].innerHTML= Number(qte[i].innerHTML)+1
+        calcul();
+    })
+}
+
+for(let i=0; i<btnminus.length;i++){
+    btnminus[i].addEventListener('click',function(){
+        if (Number(qte[i].innerHTML)>0){
+        qte[i].innerHTML= Number (qte[i].innerHTML)-1
+        calcul();
+    }
+    })
+
+}
+
+for(let i=0;i<heart.length;i++){
+    heart[i].addEventListener('click',function(){
+          heart[i].classList.toggle(`fas`)
+    })
+}   
+ 
